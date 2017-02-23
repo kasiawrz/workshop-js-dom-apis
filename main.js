dac9fc3bdac0ff58647fd75149c77c845f957d1f
Konrad Dzwinel <kdzwinel@gmail.com>
2017-02-23 10:40:58 +0100

    Result

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const moviesAPIPath = 'https://api.themoviedb.org/3';
 const moviesAPIMethod = '/movie/popular';
 const moviesAPIKey = '46270c06c4dcf45c52b9f26efbe2841d';
 
+const giphyAPIPath = 'http://api.giphy.com/v1';
+const giphyAPIMethod = '/gifs/search';
+const giphyAPIKey = 'dc6zaTOxFJmzC';
+
 const ul = document.querySelector('ul');
 
 const url = new URL(moviesAPIPath + moviesAPIMethod);
@@ -15,8 +19,30 @@ function showListOfMovies(data) {
   (data.results).forEach(movie => {
     const li = document.createElement('li');
     const text = document.createTextNode(movie.title);
+    const button = document.createElement('button');
+    button.innerHTML = 'See trailer';
+    button.addEventListener('click', () => showTrailer(li, movie.title));
 
     li.appendChild(text);
+    li.appendChild(button);
     ul.appendChild(li);
   });
+}
+
+function showTrailer(li, title) {
+  const url = new URL(giphyAPIPath + giphyAPIMethod);
+  url.searchParams.append('api_key', giphyAPIKey);
+  url.searchParams.append('q', '"' + title + '" trailer');
+  url.searchParams.append('limit', 1);
+
+  fetch(url)
+    .then(result => result.json())
+    .then(json => {
+      if (json.data.length) {
+        var img = document.createElement('img');
+        img.src = json.data[0].images.fixed_height.url;
+        li.appendChild(document.createElement('br'));
+        li.appendChild(img);
+      }
+    });
 }
--- readme.md
+++ readme.md
@@ -1,19 +1,3 @@
-# Workshop - Step 3
+# Workshop - Result
 
-## Expected result
-For each movie title add possibility to show a gif of a trailer
-
-<img src='http://i.imgur.com/hGvkXBO.png' alt='ouput tab on jsbin' width='444' />
-
-## Steps
-
-1. based on the previous step `*`, for each movie title on the list create a button
-1. this button, when clicked, should search for a gif trailer of that movie using Giphy API ( https://github.com/Giphy/GiphyAPI - use a public beta key )
-1. first matching gif should be displayed under the button, and the title, of the movie
-
-`*` If you haven't finished previous step, feel free to use code from main.js included here
-
-## Docs
-
-- listening for events - https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
-- Giphy API docs - https://github.com/Giphy/GiphyAPI
+main.js attached to this branch contains the result of the workshop
